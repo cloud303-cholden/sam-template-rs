1b177b37f15ce5788b32bb9828c2057d554469d9
Conner Holden <80974257+cloud303-cholden@users.noreply.github.com>
2023-06-29 09:34:51 -0600

    Update to single template structure

--- {{ cookiecutter.project_slug }}/src/main.rs
+++ {{ cookiecutter.project_slug }}/src/main.rs
@@ -1,3 +1,7 @@
+use lambda_http::{
+    Body,
+    Response,
+}
 use lambda_runtime::{
     run,
     service_fn,
@@ -10,24 +14,22 @@ use serde::{
 };
 use tracing::info;
 
-#[derive(Serialize)]
-struct Response {
-    status_code: i32,
-    body: String,
-}
-
-async fn function_handler(_event: LambdaEvent<_>) -> Result<Response, Box<dyn std::error::Error>> {
+async fn function_handler(
+    _event: LambdaEvent<_>,
+) -> Result<Response<Body>, Box<dyn std::error::Error>> {
+    // Load AWS config from environment.
     let config = ::aws_config::load_from_env().await;
 
-    let resp = Response {
-        status_code: 200,
-        body: "OK".to_string(),
-    };
-    Ok(resp)
+    // Default to an HTTP response.
+    Ok(Response::builder()
+        .status(200)
+        .body("ok".into())?
+    )
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    // Filter out config cache warning and hyper debug insanity.
     let env_filter = env::var("ENV_FILTER")
         .unwrap_or("aws_config=warn,hyper=info".to_string());
 
